f9d2dc32baaf1ec557cd94a1cad6fa3938faaf0f
soham-coder13 <soham.coder@gmail.com>
2020-07-01 13:41:13 +0530

    added Dal code for discounts

---
+++ Promotion_Engine/Dal/Promotion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    class Promotion
+    {
+        public DataTable GetDiscounts()
+        {
+            DataTable dtDiscount = new DataTable();
+
+            dtDiscount.Columns.Add("Item");
+            dtDiscount.Columns.Add("Quantity");
+            dtDiscount.Columns.Add("DiscountedPrice");
+
+            DataRow dr = dtDiscount.NewRow();
+            dr["Item"] = "A";
+            dr["Item"] = 3;
+            dr["Discount"] = 130.0;
+            dtDiscount.Rows.Add(dr);
+            dr = dtDiscount.NewRow();
+            dr["Item"] = "B";
+            dr["Item"] = 2;
+            dr["Discount"] = 45.0;
+            dtDiscount.Rows.Add(dr);
+            dr = dtDiscount.NewRow();
+            dr["Item"] = "C;D";
+            dr["Item"] = 1;
+            dr["Discount"] = 30.0;
+            dtDiscount.Rows.Add(dr);
+            dr = dtDiscount.NewRow();
+
+            return dtDiscount;
+        }
+    }
+}
