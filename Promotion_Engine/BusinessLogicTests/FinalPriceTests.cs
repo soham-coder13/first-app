5fad81609292187e1750579c4d2c462d6e0e4ef3
soham-coder13 <soham.coder@gmail.com>
2020-07-01 12:57:13 +0530

    added unit test methods and their expected results, which failed now since no logic added

--- Promotion_Engine/BusinessLogicTests/FinalPriceTests.cs
+++ Promotion_Engine/BusinessLogicTests/FinalPriceTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using CalculatePrice;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BusinessLogicTests
@@ -7,8 +9,49 @@ namespace BusinessLogicTests
     public class FinalPriceTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Scenario_A_Test()
         {
+            Dictionary<string, int> dictCart = new Dictionary<string, int>();
+            FinalPrice cal = new FinalPrice();
+
+            dictCart.Add("A", 1);
+            dictCart.Add("B", 1);
+            dictCart.Add("C", 1);
+
+            decimal expectedValue = cal.CalculatePriceAfterDiscounts(dictCart, "Apply-A");
+
+            Assert.IsTrue(expectedValue == 100.0M);
+        }
+
+        [TestMethod]
+        public void Scenario_B_Test()
+        {
+            Dictionary<string, int> dictCart = new Dictionary<string, int>();
+            FinalPrice cal = new FinalPrice();
+
+            dictCart.Add("A", 5);
+            dictCart.Add("B", 5);
+            dictCart.Add("C", 1);
+
+            decimal expectedValue = cal.CalculatePriceAfterDiscounts(dictCart, "Apply-A");
+
+            Assert.IsTrue(expectedValue == 370.0M);
+        }
+
+        [TestMethod]
+        public void Scenario_CD_Test()
+        {
+            Dictionary<string, int> dictCart = new Dictionary<string, int>();
+            FinalPrice cal = new FinalPrice();
+
+            dictCart.Add("A", 3);
+            dictCart.Add("B", 5);
+            dictCart.Add("C", 1);
+            dictCart.Add("D", 1);
+
+            decimal expectedValue = cal.CalculatePriceAfterDiscounts(dictCart, "Apply-A");
+
+            Assert.IsTrue(expectedValue == 280.0M);
         }
     }
 }
